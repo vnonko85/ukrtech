545223a714cfeb09a71a2633d563e0681f27a6e0
Nonko Vlad <v.nonko85@gmail.com>
2019-09-18 04:11:01 +0300

    fix environment

--- common/config/main.php
+++ common/config/main.php
@@ -6,13 +6,6 @@ return [
     ],
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
-        'db' => [
-            'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=tz',
-            'username' => 'root',
-            'password' => 'Q1w2e3r4!',
-            'charset' => 'utf8',
-        ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
